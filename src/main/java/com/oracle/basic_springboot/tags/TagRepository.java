a23c26deb36761c908153ad824d0662b0ef80177
norman_japheth_aberin <norman.japheth.aberin@oracle.com>
2025-02-03 01:00:24 -0600

    Removed annotation

--- src/main/java/com/oracle/basic_springboot/tags/TagRepository.java
+++ src/main/java/com/oracle/basic_springboot/tags/TagRepository.java
@@ -2,9 +2,7 @@ package com.oracle.basic_springboot.tags;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TagRepository extends JpaRepository<Tag, Integer> {
 
 
